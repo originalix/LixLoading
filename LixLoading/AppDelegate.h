b47d0fc1580e96da0ca6342c8a1676a4a874455d
originalix <77252102@qq.com>
2016-07-15 10:36:29 +0800

    Initial Commit

---
+++ LixLoading/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LixLoading
+//
+//  Created by Lix on 16/7/15.
+//  Copyright © 2016年 Lix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LixLoading/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LixLoading
+//
+//  Created by Lix on 16/7/15.
+//  Copyright © 2016年 Lix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
